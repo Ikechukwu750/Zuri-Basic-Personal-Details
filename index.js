0c2f3477fe090f4b509fe9bb877e7c01008c8db4
Ikechukwu Kalu <71293780+Ikechukwu750@users.noreply.github.com>
2022-06-21 23:46:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let firstName = "Ikechukwu";
+let lastName = "Kalu";
+let height = "1.8m";
+let country = "Nigeria";
+
+let fullName = firstName + ' ' + lastName + "\n";
+
+document.write('Name: ' + fullName + "<br>");
+document.write('Height: ' + height + "<br>");
+document.write('Country: ' + country + "<br>");
