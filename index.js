76a540b0f0327dd3f6f91424863caabcdcd953d7
Ikechukwu Kalu <71293780+Ikechukwu750@users.noreply.github.com>
2022-06-22 22:30:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ let fullName = firstName + ' ' + lastName + "\n";
 document.write('Name: ' + fullName + "<br>");
 document.write('Height: ' + height + "<br>");
 document.write('Country: ' + country + "<br>");
+
+alert('My name is Ikechukwu' + '<br>' + 'My height is 1.8m' + '<br>' + 'I am from Nigeria.')
